c7e61f4e1d77c6113dac472e1f038a9a9faaba35
shipshapebear <shipshapebear@gmail.com>
2023-08-16 03:46:52 +0800

    added back to top on refresh

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -5,4 +5,16 @@ import './src/styles/global.css'
 export const wrapPageElement = ({ element, }) => {
   return <GlobalProvider>{element}</GlobalProvider>
 }
+export const shouldUpdateScroll = () => false;
 
+// export const shouldUpdateScroll = ({
+//   routerProps: { location },
+//   getSavedScrollPosition
+// }) => {
+//   const currentPosition = getSavedScrollPosition(location)
+//   const queriedPosition = getSavedScrollPosition({ pathname: `/random` })
+
+//   window.scrollTo(...(currentPosition || [0, 0]))
+
+//   return false
+// }
